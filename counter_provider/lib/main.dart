3b55ce58ef04a37cc063e33bd2cd2256d7bdf91d
L-nsamba <l.nsamba@alustudent.com>
2026-09-26 10:30:32 +0200

    counter provider session 1

--- counter_provider/lib/main.dart
+++ counter_provider/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:counter_provider/providers/CounterModel.dart';
+import 'package:counter_provider/providers/counter_model.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
